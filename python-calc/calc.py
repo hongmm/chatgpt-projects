e2cf3168e109352488652cba4f367a0ebd9bac1e
Matt Hong <matthew.m.hong@gmail.com>
2023-05-17 15:13:30 -0700

    simple calculator
    quick intro project into python
    created a basic calculator using python to handle simple calculations

---
+++ python-calc/calc.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    print("Invalid operator.")
+    # Add code here to handle invalid operator input
+    
+print("Result: " + str(result))
